b681628513f7fa6e7b55b27f2dffa3ffceafbf48
Prince kumar <snyderprince@gmail.com>
2025-09-14 23:19:53 +0530

    REACT QUIZ

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './component/Quiz'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
